49f382e3565a71108a5a2705fafa0d2afe92ca8b
DenisTunyk <tundenis43@gmail.com>
2023-02-08 17:04:53 +0200

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -72,87 +72,8 @@ export const App = () => {
           img={imgLarge}
           showModal={showModal}
           setShowModal={setShowModal}
-        >
-          {/* {this.props.children} */}
-        </Modal>
+        ></Modal>
       )}
     </div>
   );
 };
-
-// export class App extends Component {
-//   state = {
-//     page: 1,
-//     query: '',
-//     items: [],
-//     isLoader: false,
-//     showModal: false,
-//     imgLarge: '',
-//     showButton: false,
-//   };
-
-//   toogleModal = (imgLarge = '') => {
-//     this.setState(state => ({
-//       showModal: !state.showModal,
-//       imgLarge,
-//     }));
-//   };
-
-//   getQuery = query => {
-//     if (query !== this.state.query) {
-//       this.setState({
-//         page: 1,
-//         query: query,
-//         items: [],
-//       });
-//     }
-//   };
-
-//   loadMore = () => {
-//     this.setState(prevState => ({
-//       page: prevState.page + 1,
-//     }));
-//   };
-
-//   async componentDidUpdate(_, prevState) {
-//     if (
-//       prevState.page !== this.state.page ||
-//       prevState.query !== this.state.query
-//     ) {
-//       this.setState({ isLoader: true });
-//       try {
-//         const data = await fetchPictureQuery(this.state.query, this.state.page);
-//         this.setState(prevState => ({
-//           items: [...prevState.items, ...data.hits],
-//         }));
-//         if (this.state.page < Math.ceil(data.totalHits / 12)) {
-//           this.setState({ showButton: true });
-//         } else {
-//           this.setState({ showButton: false });
-//         }
-//       } catch (error) {
-//         console.log(error);
-//       } finally {
-//         this.setState({ isLoader: false });
-//       }
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <Searchbar onSubmit={this.getQuery} />
-//         <ImageGallery query={this.state.items} openModal={this.toogleModal} />
-//         {!(this.state.items.length === 0) &&
-//           this.state.showButton &&
-//           !this.state.isLoader && <Button onClick={this.loadMore} />}
-//         <Loader isLoader={this.state.isLoader} />
-//         {this.state.showModal && (
-//           <Modal onClose={this.toogleModal} img={this.state.imgLarge}>
-//             {this.props.children}
-//           </Modal>
-//         )}
-//       </div>
-//     );
-//   }
-// }
